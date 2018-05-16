aa9894d79b501c63babfd4f45d666ba3a34c85e0
zfreedman <zfreedman92@gmail.com>
2018-05-16 15:57:02 -0700

    feat: Creating new state from newly fetched weather data

--- intermediate_redux_middleware/src/reducers/reducer_weather.js
+++ intermediate_redux_middleware/src/reducers/reducer_weather.js
@@ -1,3 +1,13 @@
-export default function(state=null, action) {
+import { FETCH_WEATHER } from "../actions/index";
+
+export default function(state=[], action) {
+  // handle FETCH_WEATHER
+  switch (action.type) {
+    case FETCH_WEATHER:
+      // method a
+      return state.concat([action.payload.data]);
+      // method b
+      return [action.payload.data, ...state];
+  }
   return state;
 }
