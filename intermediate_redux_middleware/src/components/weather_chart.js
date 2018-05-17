58b7902ed9ad82269b12a906e37ac9f3e7712bf3
zfreedman <zfreedman92@gmail.com>
2018-05-17 16:30:36 -0700

    style: Added styling to style.css to fix chart size differences

--- intermediate_redux_middleware/src/components/weather_chart.js
+++ intermediate_redux_middleware/src/components/weather_chart.js
@@ -10,7 +10,7 @@ function average(data) {
 export default (props) => {
   return (
     <div>
-      <Sparklines height={120} width={120} data={props.data}>
+      <Sparklines height={120} width={180} data={props.data}>
         <SparklinesLine color={props.color} />
         <SparklinesReferenceLine type="avg" />
       </Sparklines>
