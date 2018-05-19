84694c673239b16b48b0910e83010369fcc44e54
zfreedman <zfreedman92@gmail.com>
2018-05-19 15:35:04 -0700

    feat: Implemented dummy application using react router

--- react_router_redux_form/src/index.js
+++ react_router_redux_form/src/index.js
@@ -1,65 +1,31 @@
-// import
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter, Route } from "react-router-dom";
 
-// youtube search
-import YTSearch from "youtube-api-search";
-// lodash for throttling
-import _ from "lodash";
+import App from "./components/app";
+import reducers from "./reducers";
 
-// custom components
-import SearchBar from "./components/search_bar";
-import VideoList from "./components/video_list";
-import VideoDetail from "./components/video_detail";
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-// api key
-const API_KEY = "AIzaSyCGtIV4hFOR2jXiuJoCWtRk-PmjA_jFnSE";
-
-// create new copmonenet to produce html
-class App extends Component {
-  // constructor
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      videos: [],
-      selectedVideo: null
-    };
-
-    // initial serach
-    this.videoSearch("surfboards");
-  }
-
-  // implementing video search
-  videoSearch(term) {
-    // make search for surf boards
-    YTSearch({key: API_KEY, term: term}, (videos) => {
-      // setting state (cause rerender)
-      // this.setState({ videos: videos })
-      this.setState({
-        //equivalent in ES6 (names have to match)
-        videos,
-        selectedVideo: videos[0]
-      })
-    });
-  }
-
-  // rendering
-  // passing data from the parent state to the child state (passing props)
-  render() {
-    const videoSearch = _.debounce((term) => {this.videoSearch(term) }, 300);
-
-    return (
-      <div>
-        <SearchBar onSearchTermChange={videoSearch}/>
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          videos={this.state.videos}
-          onVideoSelect={selectedVideo => this.setState({selectedVideo})}/>
-      </div>
-    );
-  }
+class Hello extends React.Component {
+  render() { return <div>Hello!</div> }
 }
 
-// put component in dom
-ReactDOM.render(<App />, document.querySelector(".container"));
+class Bye extends React.Component {
+  render() { return <div>Bye!</div> }
+}
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <App />
+        <Route path="/hello" component={Hello} />
+        <Route path="/bye" component={Bye} />
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector(".container")
+);
