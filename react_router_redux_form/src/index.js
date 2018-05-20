1d4ffd6b4c0a23295c1f2553a8a66d159b80d313
zfreedman <zfreedman92@gmail.com>
2018-05-20 15:00:50 -0700

    feat: Rendering starter posts component using react router for '/' url

--- react_router_redux_form/src/index.js
+++ react_router_redux_form/src/index.js
@@ -4,26 +4,16 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter, Route } from "react-router-dom";
 
-import App from "./components/app";
 import reducers from "./reducers";
+import PostsIndex from "./components/posts_index";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-class Hello extends React.Component {
-  render() { return <div>Hello!</div> }
-}
-
-class Bye extends React.Component {
-  render() { return <div>Bye!</div> }
-}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <App />
-        <Route path="/hello" component={Hello} />
-        <Route path="/bye" component={Bye} />
+        <Route path="/" component={PostsIndex} />
       </div>
     </BrowserRouter>
   </Provider>,
