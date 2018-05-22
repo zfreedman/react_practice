9bfd50eb4728b586557061296732a444dd8da179
zfreedman <zfreedman92@gmail.com>
2018-05-21 19:00:16 -0700

    feat: Rendering posts iteratively based on updated PostsIndex state

--- react_router_redux_form/src/components/posts_index.js
+++ react_router_redux_form/src/components/posts_index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import _ from "lodash";
 import { fetchPosts } from "../actions";
 
 class PostsIndex extends Component {
@@ -8,13 +9,30 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      );
+    });
+  }
+
   render() {
     return (
       <div>
-      Posts Index
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
 }
 
-export default connect(null, { fetchPosts })(PostsIndex);
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
