8548b06db3b388ad221c7540d45cd10e4fb7ac65
zfreedman <zfreedman92@gmail.com>
2018-05-21 19:55:50 -0700

    feat: Rendering fields using redux-form Field

--- react_router_redux_form/src/components/posts_new.js
+++ react_router_redux_form/src/components/posts_new.js
@@ -1,13 +1,34 @@
 import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
 
 class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          placeholder={field.label}
+          type="text"
+          {...field.input} />
+      </div>
+    );
+  }
+
   render() {
     return (
-      <div>
-        PostsNew
-      </div>
+      <form>
+        <Field label="Title" name="title" component={this.renderField} />
+        <Field label="Tags" name="tags" component={this.renderField} />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField} />
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: "PostsNewForm"
+})(PostsNew);
