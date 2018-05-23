12df2009d187733c5ebf8e12507f977f8b2ecb5b
zfreedman <zfreedman92@gmail.com>
2018-05-22 18:14:54 -0700

    feat: Added cancel form button to PostsNew form

--- react_router_redux_form/src/components/posts_new.js
+++ react_router_redux_form/src/components/posts_new.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
+import { Link } from "react-router-dom";
 
 class PostsNew extends Component {
   renderField(field) {
@@ -50,6 +51,7 @@ class PostsNew extends Component {
           name="content"
           component={this.renderField} />
         <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     );
   }
