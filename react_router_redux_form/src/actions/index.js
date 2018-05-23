114533e998265546c4f261bb2c86bf7e56600d39
zfreedman <zfreedman92@gmail.com>
2018-05-22 18:38:27 -0700

    feat: Successfully posting data to backend

--- react_router_redux_form/src/actions/index.js
+++ react_router_redux_form/src/actions/index.js
@@ -1,16 +1,28 @@
 import axios from "axios";
 
 export const FETCH_POSTS = "fetch_posts";
+export const CREATE_POST = "create_post";
 
 // ALL requests require a key parameter, eg `reduxblog.herokuapp.com/api/posts?key=123`
 const ROOT_URL = "http://reduxblog.herokuapp.com/api";
 const API_KEY = '?key=something12345';
 
+// action used for fetching posts
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
   return {
     type: FETCH_POSTS,
+    payload: request
+  };
+}
+
+// acting used for creating new posts
+export function createPost(values) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values);
+
+  return {
+    type: CREATE_POST,
     payload: request
   };
 }
