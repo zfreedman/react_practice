9870f57baa60613e7d73f86127d165ff39d15176
zfreedman <zfreedman92@gmail.com>
2018-05-23 17:24:34 -0700

    feat: Implemented post deletion

--- react_router_redux_form/src/actions/index.js
+++ react_router_redux_form/src/actions/index.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export const FETCH_POSTS = "fetch_posts";
 export const CREATE_POST = "create_post";
 export const FETCH_POST = "fetch_post";
+export const DELETE_POST = "delete_post";
 
 // ALL requests require a key parameter, eg `reduxblog.herokuapp.com/api/posts?key=123`
 const ROOT_URL = "http://reduxblog.herokuapp.com/api";
@@ -29,12 +30,23 @@ export function createPost(values, callback) {
   };
 }
 
-
+// used to get a particular post
 export function fetchPost(post_id) {
   const request = axios.get(`${ROOT_URL}/posts/${post_id}${API_KEY}`);
 
   return {
     type: FETCH_POST,
     payload: request
+  };
+}
+
+// used to delete a particular post
+export function deletePost(post_id, callback) {
+  const request = axios.delete(`${ROOT_URL}/posts/${post_id}${API_KEY}`)
+    .then(() => callback());
+
+  return {
+    type: DELETE_POST,
+    payload: post_id
   };
 }
