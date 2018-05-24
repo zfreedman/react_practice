9353a8e7b28e59500ce2e9f2e8c1c1de0b197405
zfreedman <zfreedman92@gmail.com>
2018-05-23 17:03:57 -0700

    feat: Added conditional rendering to PostsShow component

--- react_router_redux_form/src/components/posts_show.js
+++ react_router_redux_form/src/components/posts_show.js
@@ -1,14 +1,36 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchPost } from "../actions";
 
 class PostsShow extends Component {
+  componentDidMount() {
+    // get access to :id in url
+    // this.props.match.params.id;
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
+  }
 
   render() {
+    const { post } = this.props;
+    // handle first render (before post is retrieved)
+    if (!post) {
+      return <div>Loading...</div>
+    }
+    // handle rednering once post is fetched
     return (
       <div>
-        Posts Show
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
   }
 }
 
-export default PostsShow;
+// ownProps is the props object going to this (PostsShow) component
+// ownProps === this.props
+function mapStateToProps({ posts }, ownProps) {
+  return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
